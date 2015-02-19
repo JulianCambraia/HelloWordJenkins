1a6f822c80d631f45875b3ad9a322951102c1317
JulianFernando <juliancambraia@gmail.com>
2015-02-19 00:30:55 -0200

    Projeto Hello Word Maven Jenkins

---
+++ src/main/java/br/com/example/hellowordmaven/HelloWordMavenMain.java
@@ -0,0 +1,15 @@
+package br.com.example.hellowordmaven;
+
+import java.util.logging.Logger;
+
+import org.slf4j.LoggerFactory;
+
+public class HelloWordMavenMain {
+
+	public static void main(String[] args) {
+		Logger logger = (Logger) LoggerFactory.getLogger(HelloWordMavenMain.class);
+		logger.info("Hello Word Maven!");
+
+	}
+
+}
